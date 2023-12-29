0652824b1cae9f8b1e1afc3d79887ec2077a41ea
Jason Pena <jasonpena@awkless.com>
2023-12-28 18:03:20 -0700

    feat: Apply pixel blur patch
    https://tools.suckless.org/slock/patches/blur-pixelated-screen/
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # slock version
-VERSION = 1.5
+VERSION = 1.5.0-patch.1
 
 # Customize below to fit your system
 
@@ -12,11 +12,11 @@ X11LIB = /usr/X11R6/lib
 
 # includes and libs
 INCS = -I. -I/usr/include -I${X11INC}
-LIBS = -L/usr/lib -lc -lcrypt -L${X11LIB} -lX11 -lXext -lXrandr
+LIBS = -L/usr/lib -lc -lcrypt -L${X11LIB} -lX11 -lXext -lXrandr -lImlib2
 
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE -DHAVE_SHADOW_H
-CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+CFLAGS = -std=c99 -pedantic -Wall -Ofast ${INCS} ${CPPFLAGS}
 LDFLAGS = -s ${LIBS}
 COMPATSRC = explicit_bzero.c
 
