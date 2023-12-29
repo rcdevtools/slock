dce63cbd6a1658b601f2c3dc325013c9b483a65f
Jason Pena <jasonpena@awkless.com>
2023-12-28 18:23:17 -0700

    feat: Setup failure command
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

---
+++ config.h
@@ -0,0 +1,24 @@
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* number of failed password attempts until failcommand is executed.
+   Set to 0 to disable */
+static const int failcount = 3;
+
+/* command to be executed after [failcount] failed password attempts */
+static const char *failcommand = "shutdown -h now";
+
+/*Enable blur*/
+#define BLUR
+/*Set blur radius*/
+static const int blurRadius=5;
+/*Enable Pixelation*/
+//#define PIXELATION
+/*Set pixelation radius*/
+static const int pixelSize=0;
